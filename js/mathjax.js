47b3d6e50f4300fbfd9bc1f5897a40af642df2f8
VicentSanchis <vsanchis.inf@gmail.com>
2024-03-06 08:52:27 +0100

    Activat mode fosc

--- js/mathjax.js
+++ js/mathjax.js
@@ -1,14 +1,27 @@
-window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
-    }
-  };
-  
+// Detecta canvis en el mode clar/fosc
+const detectThemeChange = () => {
+  const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  applyMermaidStyles(theme);
+};
+
+// Aplica els estils Mermaid corresponents
+const applyMermaidStyles = (theme) => {
+  const mermaidDiagram = document.querySelector('.mermaid');
+  if (mermaidDiagram) {
+      if (theme === 'dark') {
+          mermaidDiagram.style.backgroundColor = 'black';
+          // Altres estils per al mode fosc
+      } else {
+          mermaidDiagram.style.backgroundColor = 'white';
+          // Altres estils per al mode clar
+      }
+  }
+};
+
+// Executa la detecció inicial del tema
+detectThemeChange();
+
+// Escolta els canvis en el tema
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', detectThemeChange);
+
   
